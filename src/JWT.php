f1ba9899b8f0c785392800e2f06d5847bf825c41
默认用户 <1046512080@qq.com>
2025-08-14 10:41:34 +0800

    Update JWT.php
    类型错误

--- src/JWT.php
+++ src/JWT.php
@@ -173,7 +173,7 @@ class JWT
      * @throws JWTTokenException
      * @author Tinywan(ShaoBo Wan)
      */
-    public static function verify(int $tokenType = self::ACCESS_TOKEN, string $token = null): array
+    public static function verify(int $tokenType = self::ACCESS_TOKEN, string|null $token = null): array
     {
         try {
             $token = $token ?? self::getTokenFromHeaders();
